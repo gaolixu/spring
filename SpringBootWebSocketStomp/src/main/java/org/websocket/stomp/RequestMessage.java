aba25b466b2ba300c6e4704e663bf27b9d5fb246
lixu.gao <lixu.gao@symbio.com>
2018-03-30 11:38:12 +0800

    spring boot websocket with stomp

---
+++ SpringBootWebSocketStomp/src/main/java/org/websocket/stomp/RequestMessage.java
@@ -0,0 +1,11 @@
+package org.websocket.stomp;
+
+
+public class RequestMessage {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ SpringBootWebSocketStomp/src/main/java/org/websocket/stomp/WsController.java
@@ -0,0 +1,16 @@
+package org.websocket.stomp;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class WsController {
+    @MessageMapping("/welcome")
+    @SendTo("/topic/getResponse")
+    public ResponseMessage say(RequestMessage message) {
+        System.out.println(message.getName());
+        return new ResponseMessage("welcome," + message.getName() + " !");
+    }
+}
